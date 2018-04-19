b6e72d5a8a82c909024bd1384c729c3ac7436898
Takeshi Fujimoto <tfujimoto1223@gmail.com>
2018-04-19 16:54:43 +0900

    Modified runtest.sh

--- XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/runtest.sh
+++ XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/runtest.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo 'Output path = ' $1
-echo 'Root Directory = ' $2
 TEST_RESULT=$1/test_results.xml
-ROOT_DIRECTORY=$2
+ROOT_DIRECTORY=$1
 echo 'Delete test result'
 rm -rf $TEST_RESULT
 mono --debug $ROOT_DIRECTORY/XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/Touch.Server.exe \
