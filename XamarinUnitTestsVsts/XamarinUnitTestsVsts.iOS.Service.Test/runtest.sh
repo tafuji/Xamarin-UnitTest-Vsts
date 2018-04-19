88275b898ac07a503bd4b43fda384545dc64c2fe
Takeshi Fujimoto <tfujimoto1223@gmail.com>
2018-04-19 17:23:09 +0900

    Modified tuntest.sh

--- XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/runtest.sh
+++ XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/runtest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo 'Output path = ' $1
+echo $NUNIT_ENABLE_XML_OUTPUT
 ROOT_DIRECTORY=$1
 TEST_PROJECT_DIRECTORY=$1/$2
 TEST_RESULT=$TEST_PROJECT_DIRECTORY/test_results.xml
@@ -8,7 +9,7 @@ rm -rf $TEST_RESULT
 mono --debug $TEST_PROJECT_DIRECTORY/Touch.Server.exe \
 --launchsim  ./bin/iPhoneSimulator/Debug/XamarinUnitTestsVsts.iOS.Service.Test.app \
 -autoexit \
--skipheader \
+-skipheader=$NUNIT_SKIP_LOG_HEADER \
 -logfile=$TEST_RESULT \
 --verbose \
- --device=":v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-3,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-8"
+--device=":v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-3,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-8"
