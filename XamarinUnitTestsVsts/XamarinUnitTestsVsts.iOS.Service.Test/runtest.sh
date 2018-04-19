53cea5ba45772a63fd154ac8202df17ba5bc4c3c
Takeshi Fujimoto <tfujimoto1223@gmail.com>
2018-04-19 17:27:05 +0900

    Modified Project settings and runtest.sh

--- XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/runtest.sh
+++ XamarinUnitTestsVsts/XamarinUnitTestsVsts.iOS.Service.Test/runtest.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo 'Output path = ' $1
-echo $NUNIT_ENABLE_XML_OUTPUT
 ROOT_DIRECTORY=$1
 TEST_PROJECT_DIRECTORY=$1/$2
 TEST_RESULT=$TEST_PROJECT_DIRECTORY/test_results.xml
@@ -9,7 +8,7 @@ rm -rf $TEST_RESULT
 mono --debug $TEST_PROJECT_DIRECTORY/Touch.Server.exe \
 --launchsim  ./bin/iPhoneSimulator/Debug/XamarinUnitTestsVsts.iOS.Service.Test.app \
 -autoexit \
--skipheader=$NUNIT_SKIP_LOG_HEADER \
+-skipheader \
 -logfile=$TEST_RESULT \
 --verbose \
 --device=":v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-3,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-8"
