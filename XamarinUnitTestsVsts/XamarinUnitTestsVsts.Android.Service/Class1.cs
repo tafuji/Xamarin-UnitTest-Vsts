a0e268922607918a921489d0539aad0218b28940
Takeshi Fujimoto <tfujimoto1223@gmail.com>
2018-04-19 14:07:10 +0900

    Added Initial solution.

---
+++ XamarinUnitTestsVsts/XamarinUnitTestsVsts.Android.Service/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamarinUnitTestsVsts.Android.Service
+{
+    public class Class1
+    {
+    }
+}
