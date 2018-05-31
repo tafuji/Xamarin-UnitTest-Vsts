7e00fc8b3895f41532f8005b36ae413e09f0297f
Takeshi Fujimoto <tfujimoto1223@gmail.com>
2018-05-31 13:23:42 +0900

    modified bat file.

--- XamarinUnitTestsVsts/XamarinUnitTestsVsts.Android.Service.Test/runtest.bat
+++ XamarinUnitTestsVsts/XamarinUnitTestsVsts.Android.Service.Test/runtest.bat
@@ -3,4 +3,4 @@ echo 'TestToolPath = ' %2
 echo  'adbpath=' = %3
 set TEST_RESULT=%2/test_results.xml
 
-%2/Touch.Server.Android.exe --ip=192.168.0.1 --activity=%1 --logfile=TEST_RESULT --adbpath=%3
+%2/Touch.Server.Android.exe --ip=192.168.0.1 --activity=%1 --logfile=%TEST_RESULT% --adbpath=%3
